64e7855536d8ccde3927c4c5264267c41285e2fd
RyanWilcox <61204543+wilcoxr3@users.noreply.github.com>
2021-03-05 22:41:34 -0500

    added rest api template

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
@@ -9,3 +11,19 @@ const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+  });
+   
+  app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+   
+  app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+  });
+   
+  app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+  });
