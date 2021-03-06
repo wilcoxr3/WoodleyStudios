864a08f72eebbe2a3ced4f9a1987952f946e57d2
RyanWilcox <61204543+wilcoxr3@users.noreply.github.com>
2021-03-06 14:56:16 -0500

    express

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const url = require('url');
+const https = require('https');
+const mongoclient = require('mongodb').MongoClient; //database
 
-const server = http.createServer((request, response) => {
+/* const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-});
+}); */
 app.get('/', (req, res) => {
-    return res.send('Received a GET HTTP method');
+    res.sendFile(path.join(__dirname + '/index.html'));
   });
    
   app.post('/', (req, res) => {
@@ -21,9 +24,39 @@ app.get('/', (req, res) => {
   app.delete('/', (req, res) => {
     return res.send('Received a DELETE HTTP method');
   });
-
-const port = process.env.PORT || 1337;
-server.listen(port);
+//USERS
+app.get('/users', (req, res) => {
+    return res.send(Object.values(users));
+  });
+   
+  app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user resource');
+  });
+   
+  app.put('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+  });
+   
+  app.delete('/users/:userId', (req, res) => {
+    return res.send(
+      `DELETE HTTP method on user/${req.params.userId} resource`,
+    );
+  });
+  let users = {
+    1: {
+      id: '1',
+      username: 'Ryan Wilcox',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+  };
+const port = process.env.PORT || 4000;
+app.listen(port, function(){
+    initialize();
+    console.log("listening on port 4000")
+});
 
 console.log("Server running at http://localhost:%d", port);
 
