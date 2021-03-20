1fd92f6a08ff01dd8b447cb29bf39c35e0ef4e72
RyanWilcox <61204543+wilcoxr3@users.noreply.github.com>
2021-03-20 17:09:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 const express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var multer = require('multer');
 
-const app = express();
+var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
 app.get('/', function(req, res) {
-  res.sendFile( __dirname + "/" + "index.htm" );
+  res.sendFile( "/public/index.htm" );
 })
-
-app.get('/index.htm', function (req, res) {
-  res.sendFile( __dirname + "/" + "index.htm" );
-})
-
-app.get('/process_get', function (req, res) {
-  // Prepare output in JSON format
-  response = {
-     first_name:req.query.first_name,
-     last_name:req.query.last_name
-  };
-  console.log(response);
-  res.end(JSON.stringify(response));
-})
-
 // This responds a POST request for the homepage
 app.post('/', function (req, res) {
   console.log("Got a POST request for the homepage");
@@ -36,12 +20,6 @@ app.post('/', function (req, res) {
 app.delete('/del_user', function (req, res) {
   console.log("Got a DELETE request for /del_user");
   res.send('Hello DELETE');
-})
-
-// This responds a GET request for the /list_user page.
-app.get('/list_user', function (req, res) {
-  console.log("Got a GET request for /list_user");
-  res.send('Page Listing');
 })
 
 var server = app.listen(8081, function () {
